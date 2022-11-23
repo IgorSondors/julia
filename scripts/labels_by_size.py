93cfb58375b26943bbe2f51521888323164a919a
IgorSondors <igor.sondors@gmail.com>
2022-11-23 16:58:37 +0000

    sizes hist

--- scripts/labels_by_size.py
+++ scripts/labels_by_size.py
@@ -1,7 +1,19 @@
 import pandas as pd
+import os
 
-csv_train = '/mnt/data/lossless_train_04102022_crops.csv'
-csv_test = '/mnt/data/lossless_val_04102022_crops.csv'
+csv_train = '/mnt/data/lossless_train_04102022_crops_sizes.csv'
+csv_test = '/mnt/data/lossless_val_04102022_crops_sizes.csv'
 
-df = pd.read_csv(csv_test, sep=',')
-print(df)
+def write_sizes(csv):
+    df = pd.read_csv(csv, sep=',')
+    print(df)
+
+    for index, row in df.iterrows():
+        file = row['file']
+        size = os.path.getsize(file)
+        df.loc[index, 'size'] = size
+
+    df.to_csv('/mnt/data/lossless_train_04102022_crops_sizes.csv', sep=',',index=False)
+    return df
+
+#write_sizes(csv_train)
